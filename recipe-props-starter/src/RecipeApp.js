0a73d652dbae9ed49a966152f74c766e8cea4d94
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-19 23:26:41 +0000

    added navbar

--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './RecipeApp.css';
 import Recipe from './Recipe';
+import Navbar from './Navbar';
 
 class RecipeApp extends Component {
   render() {
