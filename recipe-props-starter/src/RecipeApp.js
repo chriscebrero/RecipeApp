d58961a5c71324ccaa7b0741c5dc4761b6e9f13a
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-20 05:25:00 +0000

    handle save

--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -10,21 +10,21 @@ class RecipeApp extends Component {
     this.state = {
       recipes: [
         {   
-            id: 1,
+            id: 0,
             title: "Cheesy Potatos",
             instructions: "Put Cheese all over the potatos, then grill it",
             ingredients: ["Cheese", "Potatos"],
             img: "CheesyPotatos.jpg"
         },
         {
-            id: 2,
+            id: 1,
             title: "Salad",
             instructions: "Mix croutons, parmesan, dressing all over",
             ingredients: ["Salad", "Croutons", "Parmesan Cheese", "Caesar Dressing"],
             img: "Salad.jpeg"
         },
         {
-            id: 3,
+            id: 2,
             title: "Pizza",
             instructions: "Put Pizza in the oven and heat it to 450 Fahrenheit for 20 minutes",
             ingredients: ["Frozen Pizza", "Extra Toppings"],
@@ -33,6 +33,10 @@ class RecipeApp extends Component {
         ],
         nextRecipeId: 3,
     }
+  }
+  
+  handleSave(recipe) {
+    
   }
   render() {
     return (
