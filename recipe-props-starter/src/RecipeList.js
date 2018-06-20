6f790b64696f191a8c1952cb1962cf2b6b1c417a
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-20 00:44:07 +0000

    added more recipes

---
+++ recipe-props-starter/src/RecipeList.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import Recipe from './Recipe';
+import 'RecipeList.css';
+import PropTypes from 'prop-types';
+
+class RecipeList extends Component {
+    static defaultProps = {
+        recipes: [
+        {
+            title: "Cheesy Potatos",
+            instructions: "Put Cheese all over the potatos, then grill it",
+            ingredients: ["Cheese", "Potatos"],
+            img: "CheesyPotatos.jpg"
+        },
+        {
+            title: "Salad",
+            instructions: "Mix croutons, parmesan, dressing all over",
+            ingredients: ["Salad", "Croutons", "Parmesan Cheese", "Caesar Dressing"],
+            img: "Salad.jpeg"
+        },
+        {
+            title: "Pizza",
+            instructions: "Put Pizza in the oven and heat it to 450 Fahrenheit for 20 minutes",
+            ingredients: ["Frozen Pizza", "Extra Toppings"],
+            img: "Pizza.jpg"
+        }
+        ]
+        
+    }
+    
+    static propTypes = {
+        recipes: PropTypes.arrayOf(PropTypes.object)
+    }
+    render() {
+        
+    }
+}
+
+export default RecipeList;
