5a74f70c92652a8aabcc19516de7dbbec5d955e6
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-20 04:31:06 +0000

    added constructor

--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 import './RecipeApp.css';
-import Recipe from './Recipe';
 import Navbar from './Navbar';
 import RecipeList from './RecipeList';
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: [
+          {
+            title: "Cheesy Potatos",
+            instructions: "Put Cheese all over the potatos, then grill it",
+            ingredients: ["Cheese", "Potatos"],
+            img: "CheesyPotatos.jpg"
+        },
+        {
+            title: "Salad",
+            instructions: "Mix croutons, parmesan, dressing all over",
+            ingredients: ["Salad", "Croutons", "Parmesan Cheese", "Caesar Dressing"],
+            img: "Salad.jpeg"
+        },
+        {
+            title: "Pizza",
+            instructions: "Put Pizza in the oven and heat it to 450 Fahrenheit for 20 minutes",
+            ingredients: ["Frozen Pizza", "Extra Toppings"],
+            img: "Pizza.jpg"
+        }
+        ]
+    }
+  }
   render() {
     return (
       <div className="App">
--- recipe-props-starter/src/RecipeList.js
+++ recipe-props-starter/src/RecipeList.js
@@ -4,30 +4,6 @@ import './RecipeList.css';
 import PropTypes from 'prop-types';
 
 class RecipeList extends Component {
-    static defaultProps = {
-        recipes: [
-        {
-            title: "Cheesy Potatos",
-            instructions: "Put Cheese all over the potatos, then grill it",
-            ingredients: ["Cheese", "Potatos"],
-            img: "CheesyPotatos.jpg"
-        },
-        {
-            title: "Salad",
-            instructions: "Mix croutons, parmesan, dressing all over",
-            ingredients: ["Salad", "Croutons", "Parmesan Cheese", "Caesar Dressing"],
-            img: "Salad.jpeg"
-        },
-        {
-            title: "Pizza",
-            instructions: "Put Pizza in the oven and heat it to 450 Fahrenheit for 20 minutes",
-            ingredients: ["Frozen Pizza", "Extra Toppings"],
-            img: "Pizza.jpg"
-        }
-        ]
-        
-    }
-    
     static propTypes = {
         recipes: PropTypes.arrayOf(PropTypes.object).isRequired
     }
