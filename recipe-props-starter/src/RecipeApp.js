11b9911313eaf8ee78e62f3aefd731a8a5c8197a
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-20 04:59:14 +0000

    changed menu

--- recipe-props-starter/src/RecipeApp.js
+++ recipe-props-starter/src/RecipeApp.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-import './RecipeApp.css';
 import Navbar from './Navbar';
 import RecipeList from './RecipeList';
+import './RecipeApp.css';
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: [
+        {   
+            id: 1,
+            title: "Cheesy Potatos",
+            instructions: "Put Cheese all over the potatos, then grill it",
+            ingredients: ["Cheese", "Potatos"],
+            img: "CheesyPotatos.jpg"
+        },
+        {
+            id: 2,
+            title: "Salad",
+            instructions: "Mix croutons, parmesan, dressing all over",
+            ingredients: ["Salad", "Croutons", "Parmesan Cheese", "Caesar Dressing"],
+            img: "Salad.jpeg"
+        },
+        {
+            id: 3,
+            title: "Pizza",
+            instructions: "Put Pizza in the oven and heat it to 450 Fahrenheit for 20 minutes",
+            ingredients: ["Frozen Pizza", "Extra Toppings"],
+            img: "Pizza.jpg"
+        }
+        ],
+    }
+  }
   render() {
     return (
       <div className="App">
-      <Navbar />
-        <RecipeList />
+        <Navbar />
+        <RecipeList recipes= {this.state.recipes} />
       </div>
     );
   }
--- recipe-props-starter/src/RecipeList.js
+++ recipe-props-starter/src/RecipeList.js
@@ -1,47 +1,26 @@
 import React, {Component} from 'react';
 import Recipe from './Recipe';
-import './RecipeList.css';
 import PropTypes from 'prop-types';
+import './RecipeList.css';
 
 class RecipeList extends Component {
-    static defaultProps = {
-        recipes: [
-        {
-            title: "Cheesy Potatos",
-            instructions: "Put Cheese all over the potatos, then grill it",
-            ingredients: ["Cheese", "Potatos"],
-            img: "CheesyPotatos.jpg"
-        },
-        {
-            title: "Salad",
-            instructions: "Mix croutons, parmesan, dressing all over",
-            ingredients: ["Salad", "Croutons", "Parmesan Cheese", "Caesar Dressing"],
-            img: "Salad.jpeg"
-        },
-        {
-            title: "Pizza",
-            instructions: "Put Pizza in the oven and heat it to 450 Fahrenheit for 20 minutes",
-            ingredients: ["Frozen Pizza", "Extra Toppings"],
-            img: "Pizza.jpg"
-        }
-        ]
-        
-    }
-    
-    static propTypes = {
-        recipes: PropTypes.arrayOf(PropTypes.object).isRequired
-    }
-    
-    render() {
+
+  static propTypes = {
+    recipes: PropTypes.arrayOf(PropTypes.object).isRequired
+  }
+  
+  render() {
     const recipes = this.props.recipes.map((r,index) => (
       <Recipe key={index} {...r} />
     ));
-        
-        return (
-            <div className="recipe-list">
-            {recipes}
-            </div>)
-    }
+    
+    return (
+      <div className="recipe-list">
+        {recipes}
+      </div>
+    )
+  
+  }
 }
 
 export default RecipeList;
