d1c5b1a188aa0ce11ed50124054e68dcf9e46af5
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-20 05:09:57 +0000

    instructions

--- recipe-props-starter/src/RecipeInput.js
+++ recipe-props-starter/src/RecipeInput.js
@@ -9,7 +9,28 @@ class RecipeInput extends Component {
             instructions: "",
             ingredients: [''],
             img:''
-        }
+        };
+    }
+    
+    render() {
+        const {title, instructions, img, ingredients} = this.state;
+        let inputs = ingredients.map((ing, i) => (
+            <div
+                className="recipe-form-line"
+                key={`ingredients-{i}`}
+                >
+                <label>
+                    <input
+                        type="text"
+                        name={`ingredient-${i}`}
+                        value={ing}
+                        size={45}
+                        autoComplete="off"
+                        placeholder = "Ingredient"
+                        onChange={() => {}} />
+                    </label>
+                </div>
+                ));
     }
 }
 
